e9672fbdcd019b0c95c82444876ccc31aa9b809b
Ganishlab <robins.ganira@strathmore.edu>
2025-08-18 17:09:40 +0300

    teszt results

--- backend/triage/tests.py
+++ backend/triage/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import TriageCase
 
-# Create your tests here.
+User = get_user_model()
+
+class TriageCaseTest(TestCase):
+    def setUp(self):
+        self.student = User.objects.create(username='student@strathmore.edu', role='student')
+        self.case = TriageCase.objects.create(
+            student=self.student,
+            result="Refer to Mentor (Non-clinical support)",
+            gender="male"
+        )
+
+    def test_case_creation(self):
+        self.assertEqual(self.case.student.username, 'student@strathmore.edu')
+        self.assertEqual(self.case.result, "Refer to Mentor (Non-clinical support)")
+        self.assertEqual(self.case.gender, "male")
